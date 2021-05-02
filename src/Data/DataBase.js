8ec43113f6b7b8185537638dc4a2d9fd47684912
mrpmohiburrahman <35300157+mrpmohiburrahman@users.noreply.github.com>
2021-05-03 02:38:28 +0600

    add fake database

---
+++ src/Data/DataBase.js
@@ -0,0 +1,19 @@
+export default database = [{
+    "id": 1,
+    "name": "name1",
+    "gluten": "gluten1",
+    "company": "company1",
+    "ingredients":"ingredients1"
+  }, {
+    "id": 2,
+    "name": "name2",
+    "gluten": "gluten2",
+    "company": "company2",
+    "ingredients":"ingredients2"
+  }, {
+    "id": 3,
+    "name": "name3",
+    "gluten": "gluten3",
+    "company": "company3",
+    "ingredients":"ingredients3"
+  }]
