4fe794e4a02309ade0102e3d7fa8fe2d6de6b0f8
mrpmohiburrahman <35300157+mrpmohiburrahman@users.noreply.github.com>
2021-05-03 03:12:28 +0600

    remove code smell

--- src/screens/ShoppingList.js
+++ src/screens/ShoppingList.js
@@ -3,51 +3,28 @@ import {
     View,
     FlatList,
     Text,
-    TouchableOpacity,
   } from 'react-native';
   
-  export default function Shoppin({ route }) {
+  export default function ShoppingList({ route }) {
     
     const { details } = route.params;
-    const results = []          // const results = [{"company": "company1", "gluten": "gluten1", "id": 1, "ingredients": "ingredients1", "name": "name1"}]
-    results.push(details)
+    const itemInfo = []          // const results = [{"company": "company1", "gluten": "gluten1", "id": 1, "ingredients": "ingredients1", "name": "name1"}]
+    itemInfo.push(details)
     
-    const renderItem = ({ item }) => {
-      return (
-        <View>
-          <TouchableOpacity
-            style={{
-              marginLeft: 20,
-              marginRight: 20,
-              elevation: 3,
-              backgroundColor: "black",
-              borderRadius: 10,
-            }}
-            >
-            <View style={{ margin: 10 }}>
-              <Text style={{ color: "white" }}>{item.company}</Text>
-              <Text style={{ color: "white", fontWeight: '700' }}>
-                {item.name}
-              </Text>
-              <Text style={{ color: "white" }}>{item.gluten}</Text>
-              <Text style={{ color: "white" }}>{item.id}</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      );
-    };
     return (
       <View style={{ flex: 1, backgroundColor: "white" }}>
-        
-        <View style={{ flex: 1, marginTop: 20 }}>
-          <FlatList
-            data={results}
-            keyExtractor={(_, index) => index.toString()}
-            renderItem={renderItem}
-          />
-        </View>
-      
+        <FlatList
+          data={itemInfo}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+              <View>
+                <Text style={{ color: "black" }}>{item.company}</Text>
+                <Text style={{ color: "black" }}>{item.name}</Text>
+                <Text style={{ color: "black" }}>{item.gluten}</Text>
+                <Text style={{ color: "black" }}>{item.id}</Text>
+              </View>
+            )}
+        />
       </View>
-    
     );
   }
